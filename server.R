378deeef72d46c296903d2782274b6d16d8b9a61
Catie79 <dark_jewel79@hotmail.com>
2018-03-02 11:32:32 -0500

    Code load

---
+++ server.R
@@ -0,0 +1,55 @@
+#
+# 
+#
+
+library(shiny)
+library(leaflet)
+library(scales)
+
+
+
+# Define server logic required to draw a map and define text for top 15 results
+shinyServer <- function(input, output) {
+  
+  Dummy_Geo_Data <- read.csv(file = "data/DummyGeoData.csv")
+  
+  output$m <- renderLeaflet({
+    
+    m <- leaflet ()
+    m <- addTiles(m)
+    m <- addMarkers (m, lng = Dummy_Geo_Data$Longitude, lat = Dummy_Geo_Data$Latitude, popup = paste(Dummy_Geo_Data$Id,"$",Dummy_Geo_Data$Cost))
+  })
+  
+  leafletOutput('m', height=900)
+  
+  proxy <- leafletProxy("m")
+  
+  output$t <- renderText({"Please filter results"})
+  
+  output$text <- renderText ({"Top 15 Results by Cost"})
+  
+# Filter based on input, triggers on Refresh button
+  
+  observeEvent(input$do,{
+    
+    #Filter down to procedure selected
+    x <- input$Procedure
+    Dummy_Health_Geo_filter <- Dummy_Geo_Data[ which(Dummy_Geo_Data$Procedure_Desc == x),]
+    Dummy_Health_Geo_filter <- Dummy_Health_Geo_filter[order(Dummy_Health_Geo_filter$Cost),]
+    
+    #Calculate savings over most expensive location
+    y <- max(Dummy_Health_Geo_filter$Cost)
+    Dummy_Health_Geo_filter$Savings <- y - Dummy_Health_Geo_filter$Cost
+    Dummy_Health_Geo_filter$Savings <- dollar(Dummy_Health_Geo_filter$Savings)
+    Dummy_Health_Geo_filter$Cost <- dollar(Dummy_Health_Geo_filter$Cost)
+      
+    #update map
+    clearMarkers(proxy)
+    addMarkers (proxy, lng = Dummy_Health_Geo_filter$Longitude, lat = Dummy_Health_Geo_filter$Latitude, popup = paste(Dummy_Health_Geo_filter$Name,"$",Dummy_Health_Geo_filter$Cost))
+    
+    #update text
+    output$t <- renderTable({head(Dummy_Health_Geo_filter, 15)})
+    
+  })
+  
+}
---
+++ ui.R
@@ -0,0 +1,34 @@
+#
+# Appliction to display costs of procedures in Washington state, allowing users to find the cheapest location for a procedure
+#USES DUMMY DATA
+# All data in this application is aggregated, randomized, and totally fake, please do not try to find a health care provider with this information
+# Strictly for demonstration purposes
+
+library(shiny)
+library(leaflet)
+
+Dummy_Geo_Data <- read.csv(file = "data/DummyGeoData.csv")
+
+# Define UI for application that draws a map of healthcare providers
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Sample Health Providers"),
+  
+  # Sidebar with a dropdown for procedures 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "Procedure",
+                  label = "Choose a procedure:",
+                  choices = Dummy_Geo_Data$Procedure_Desc),
+      actionButton("do", "Refresh")
+    ),
+    
+    # Show map and text of results
+    mainPanel(
+      leafletOutput("m"),
+      span(textOutput ("text"),style = "font-weight: bold"),
+      tableOutput ("t")
+    )
+  )
+))
